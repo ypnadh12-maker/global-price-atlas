9b31dd09e9a7f53900bba4d135e8e809f1403a63
Yellup Nadh <yellup.nadh@valtech.com>
2026-02-11 11:41:28 +0530

    fix supabase build crash

--- app/api/click/route.ts
+++ app/api/click/route.ts
@@ -1,10 +1,15 @@
-import { supabase } from '@/lib/supabase'
+import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
 
-export async function POST(req: Request) {
+export async function POST(req) {
   const body = await req.json()
 
   console.log('CLICK API HIT', body)
+
+  const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY
+  )
 
   const { error } = await supabase
     .from('affiliate_clicks')
